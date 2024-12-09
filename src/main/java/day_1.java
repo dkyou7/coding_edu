d04d287254644963bcec12d3f2682114442ab1a7
youdongkwan <dkyou7@naver.com>
2024-12-09 11:16:19 +0900

    return 을 사용하여, 필요없는 else를 지운다. • 코드의 흐름을 더 단순화 하기 위해, Guard 절이 참이면 즉시 실행을 종료하는 코드로 리팩토링 • 이를 Replace Nested Conditional with Guard Clauses 라고 한다.

--- src/main/java/day_1.java
+++ src/main/java/day_1.java
@@ -2,14 +2,18 @@ public class day_1 {
     public static int splitAndSum(String text) {
         int result = 0;
 
+        /**
+         * return 을 사용하여, 필요없는 else를 지운다.
+         * • 코드의 흐름을 더 단순화 하기 위해, Guard 절이 참이면 즉시 실행을 종료하는 코드로 리팩토링
+         * • 이를 Replace Nested Conditional with Guard Clauses 라고 한다.
+         */
         if (text == null || text.isEmpty()) {
-            result = 0;
+            return 0;
         }
-        else {
-            String[] values = text.split("-");
-            for (String value : values) {
-                result += Integer.parseInt(value);
-            }
+
+        String[] values = text.split("-");
+        for (String value : values) {
+            result += Integer.parseInt(value);
         }
         return result;
     }
