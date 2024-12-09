722969b1d056b74a0bcd83514a40e23a89cabd9a
youdongkwan <dkyou7@naver.com>
2024-12-09 15:57:19 +0900

    [1일차-오후] cal test 추가

---
+++ src/main/java/Cal.java
@@ -0,0 +1,6 @@
+public class Cal {
+    // 테스트 대상이 되는 모듈 target 을 "SUT" 라고 한다.
+    public int getSum(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/CalTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalTest {
+
+    @Test
+    void getSum() {
+        Cal cal  = new Cal();
+        assertEquals(3,cal.getSum(1,2));
+    }
+
+    @Test
+    void getSum2() {
+        Cal cal  = new Cal();
+        assertEquals(6,cal.getSum(2,4));
+    }
+    @Test
+    void getSum3() {
+        Cal cal  = new Cal();
+        assertNotEquals(6,cal.getSum(2,3),"정답은 5입니다.");
+    }
+    @Test
+    void getSum4() {
+        Cal cal  = new Cal();
+        assertEquals(3,cal.getSum(1,2));
+    }
+    @Test
+    void getSum5() {
+        Cal cal  = new Cal();
+        assertEquals(3,cal.getSum(1,2));
+    }
+    @Test
+    void getSum6() {
+        Cal cal  = new Cal();
+        assertEquals(3,cal.getSum(1,2));
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("여기에 초기화 코드가 들어갑니다.");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("여기에 마무리 코드가 들어갑니다.");
+    }
+}
