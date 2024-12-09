6fa168b306df8d36a76ae7df747dbec5b21ad4aa
youdongkwan <dkyou7@naver.com>
2024-12-09 11:21:02 +0900

    타입을 변경하는 메서드를 구현

--- src/main/java/day_1.java
+++ src/main/java/day_1.java
@@ -1,7 +1,5 @@
 public class day_1 {
     public static int splitAndSum(String text) {
-        int result = 0;
-
         /**
          * return 을 사용하여, 필요없는 else를 지운다.
          * • 코드의 흐름을 더 단순화 하기 위해, Guard 절이 참이면 즉시 실행을 종료하는 코드로 리팩토링
@@ -12,7 +10,8 @@ public class day_1 {
         }
 
         String[] values = text.split("-");
-        result = getSum(values, result);
+        int[] numbers = toInts(values);
+        int result = getSum(numbers);
         return result;
     }
 
@@ -21,10 +20,22 @@ public class day_1 {
      * getSum 은 하나씩 수로 바꾸어 sum을 구하는 메서드
      * → 수로 변경하는 메서드 / sum을 구하는 메서드로 분리
      */
-    private static int getSum(String[] values, int result) {
-        for (String value : values) {
-            result += Integer.parseInt(value);
+    private static int getSum(int[] values) {
+        int result = 0;
+        for (int value : values) {
+            result += value;
         }
         return result;
+    }
+
+    /**
+     * 타입을 변경하는 메서드를 구현
+     */
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
     }
 }
