652f17c66e91457796f339628bec80a7ff5ac020
youdongkwan <dkyou7@naver.com>
2024-12-09 11:24:20 +0900

    추상화 Level을 맞추어준다. 더 이해하기 쉽게 변경

--- src/main/java/day_1.java
+++ src/main/java/day_1.java
@@ -1,16 +1,30 @@
 public class day_1 {
     public static int splitAndSum(String text) {
-        /**
-         * return 을 사용하여, 필요없는 else를 지운다.
-         * • 코드의 흐름을 더 단순화 하기 위해, Guard 절이 참이면 즉시 실행을 종료하는 코드로 리팩토링
-         * • 이를 Replace Nested Conditional with Guard Clauses 라고 한다.
-         */
-        if (text == null || text.isEmpty()) {
-            return 0;
-        }
-        return getSum(toInts(text.split("-")));
+        if (isEmpty(text)) return 0;
+        return getSum(toInts(textSplit(text)));
     }
-
+    /**
+     * 더 이해하기 쉽게 변경
+     */
+    private static String[] textSplit(String text) {
+        return text.split("-");
+    }
+    /**
+     * 더 이해하기 쉽게 변경
+     */
+    private static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+    /**
+     * 타입을 변경하는 메서드를 구현
+     */
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
     /**
      * 하나의 역할을 하는 메소드로 만든다.
      * getSum 은 하나씩 수로 바꾸어 sum을 구하는 메서드
@@ -22,16 +36,5 @@ public class day_1 {
             result += value;
         }
         return result;
-    }
-
-    /**
-     * 타입을 변경하는 메서드를 구현
-     */
-    private static int[] toInts(String[] values) {
-        int[] numbers = new int[values.length];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(values[i]);
-        }
-        return numbers;
     }
 }
