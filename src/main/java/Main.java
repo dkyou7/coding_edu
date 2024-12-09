80bc5859464e5426431da9cb6174e119de9abc1e
youdongkwan <dkyou7@naver.com>
2024-12-09 13:12:22 +0900

    [1일차-오후] test2 init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(day_1.splitAndSum("11-22-33"));
+//        System.out.println(day_1.splitAndSum("11-22-33"));
+
+        //25+61=100
+        //1 ~ 5자리수 덧셈 수식이 맞는지 확인하는 프로그램
+        //띄어쓰기 없음
+//        String str = "25+61=86"; //PASS
+//        String str = "12345+12345=24690"; //PASS
+//        String str = "5++5=10"; //ERROR
+        String str = "10000+1=10002"; //FAIL
+
+        System.out.println(day_1_2.getResult(str));
     }
 }
