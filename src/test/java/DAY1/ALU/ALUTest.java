363594bf423f64904da1124a78f375014096a189
youdongkwan <dkyou7@naver.com>
2024-12-09 17:07:25 +0900

    [1일차-오후] ALU Test code 추가

--- src/test/java/DAY1/ALU/ALUTest.java
+++ src/test/java/DAY1/ALU/ALUTest.java
@@ -5,30 +5,114 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ALUTest {
-
-    @Test
-    void setOperand1() {
-    }
-
-    @Test
-    void setOperand2() {
-    }
-
-    @Test
-    void setOPCODE() {
-    }
-
     @Test
     void enableSignal() {
         ALU alu = new ALU();
         alu.setOperand1(10);
         alu.setOperand2(20);
         alu.setOPCODE("ADD");
-
         Result ret = new Result();
         alu.enableSignal(ret);
-
         assertEquals(30, ret.getResult());
         assertEquals(0, ret.getStatus());
+    }
+    @Test
+    void enableSignal2() {
+        ALU alu = new ALU();
+        alu.setOperand1(10);
+        alu.setOperand2(20);
+        alu.setOPCODE("MUL");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(200, ret.getResult());
+        assertEquals(0, ret.getStatus());
+    }
+    @Test
+    void enableSignal3() {
+        ALU alu = new ALU();
+        alu.setOperand1(10);
+        alu.setOperand2(20);
+        alu.setOPCODE("SUB");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(-10, ret.getResult());
+        assertEquals(0, ret.getStatus());
+    }
+    @Test
+    void enableSignal4() {
+        ALU alu = new ALU();
+        alu.setOperand1(-1);
+        alu.setOperand2(20);
+        alu.setOPCODE("SUB");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(1, ret.getStatus());
+    }
+    @Test
+    void enableSignal5() {
+        ALU alu = new ALU();
+        alu.setOperand1(50);
+        alu.setOperand2(-1);
+        alu.setOPCODE("SUB");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(2, ret.getStatus());
+    }
+    @Test
+    void enableSignal6() {
+        ALU alu = new ALU();
+        alu.setOperand1(-1);
+        alu.setOperand2(20);
+        alu.setOPCODE("ADD");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(1, ret.getStatus());
+    }
+    @Test
+    void enableSignal7() {
+        ALU alu = new ALU();
+        alu.setOperand1(50);
+        alu.setOperand2(-1);
+        alu.setOPCODE("ADD");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(2, ret.getStatus());
+    }
+    @Test
+    void enableSignal8() {
+        ALU alu = new ALU();
+        alu.setOperand1(-1);
+        alu.setOperand2(20);
+        alu.setOPCODE("MUL");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(1, ret.getStatus());
+    }
+    @Test
+    void enableSignal9() {
+        ALU alu = new ALU();
+        alu.setOperand1(50);
+        alu.setOperand2(-1);
+        alu.setOPCODE("MUL");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(2, ret.getStatus());
+    }
+    @Test
+    void enableSignal10() {
+        ALU alu = new ALU();
+        alu.setOperand1(50);
+        alu.setOperand2(33);
+        alu.setOPCODE("RMUL");
+        Result ret = new Result();
+        alu.enableSignal(ret);
+        assertEquals(65535, ret.getResult());
+        assertEquals(3, ret.getStatus());
     }
 }
