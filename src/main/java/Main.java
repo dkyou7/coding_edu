1ad60e82f7bfba369aefa437c9f969bf4b9749a5
youdongkwan <dkyou7@naver.com>
2024-12-09 11:25:08 +0900

    리팩토링 완료.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int ret = day_1.splitAndSum("11-22-33");
-        System.out.println(ret);
+        System.out.println(day_1.splitAndSum("11-22-33"));
     }
 }
