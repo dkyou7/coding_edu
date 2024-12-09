7d6eb57b0a61972199768d0492ef23261ca42d6a
youdongkwan <dkyou7@naver.com>
2024-12-09 11:00:29 +0900

    첫번째 리팩토링 초기화

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,6 @@
 public class Main {
-
-    public static int splitAndSum(String text) {
-        int result = 0;
-
-        if (text == null || text.isEmpty()) {
-            result = 0;
-        }
-        else {
-            String[] values = text.split("-");
-            for (String value : values) {
-                result += Integer.parseInt(value);
-            }
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-
-        int ret = splitAndSum("11-22-33");
-
+        int ret = day_1.splitAndSum("11-22-33");
         System.out.println(ret);
-
     }
-
 }
