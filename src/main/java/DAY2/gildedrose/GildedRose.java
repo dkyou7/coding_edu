26b169358b85c6ade4f2a4bf3f52e5cbfd89ee89
youdongkwan <dkyou7@naver.com>
2024-12-10 15:57:12 +0900

    [2일차-오후] ✓Step 7-3. else -> Alt+Enter 후 Merge ‘else if’ ▪ java 파일 윗부분에 있는 else에 대해 리팩토링 수행

--- src/main/java/DAY2/gildedrose/GildedRose.java
+++ src/main/java/DAY2/gildedrose/GildedRose.java
@@ -35,12 +35,10 @@ class GildedRose {
                         }
                     }
                 }
+            } else if (item.name.equals(SULFURAS)) {
             } else {
-                if (item.name.equals(SULFURAS)) {
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
 
@@ -55,21 +53,14 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
-            } else {
-                if (item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.sellIn < 0) {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.name.equals(SULFURAS)) {
-                    }else{
-                        if (item.sellIn < 0) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    }
-
+            } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+            } else if (item.name.equals(SULFURAS)) {
+            } else if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
