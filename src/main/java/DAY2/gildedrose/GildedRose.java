c190174d97097c1ceacc33a5b968aa73234fc5cf
youdongkwan <dkyou7@naver.com>
2024-12-10 15:35:17 +0900

    [2일차-오후]  “||” 를 찾아, Split Condition (Split into 2 ‘if’ statements) 수행 ▪ “||” 에 블록 잡고, Alt + Enter 누른다. ▪ Unit Test를 수행하여, 리팩토링에 문제가 없음을 확인한다.

--- src/main/java/DAY2/gildedrose/GildedRose.java
+++ src/main/java/DAY2/gildedrose/GildedRose.java
@@ -15,26 +15,43 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                            if (item.name.equals(BACKSTAGE_PASSES)) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                    } else {
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            } else {
                 if (item.quality > 0) {
                     if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
