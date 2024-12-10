a4dbbbc64ec500577c4a02b88dc51876f796d8a8
youdongkwan <dkyou7@naver.com>
2024-12-10 15:48:49 +0900

    [2일차-오후] invert if condition

--- src/main/java/DAY2/gildedrose/GildedRose.java
+++ src/main/java/DAY2/gildedrose/GildedRose.java
@@ -50,19 +50,19 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.name.equals(AGED_BRIE)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        item.quality = item.quality - item.quality;
+                    } else {
                         if (item.quality > 0) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
