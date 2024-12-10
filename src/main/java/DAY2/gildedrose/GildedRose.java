975a564dc17797067bb76f43c4a38b187b07a8bd
youdongkwan <dkyou7@naver.com>
2024-12-10 15:52:04 +0900

    [2일차-오후] ✓Step 6-3. 조건문 순서 변경 → if (item.sellIn < 0) ▪ 순서 변경 후, 테스트 필수

--- src/main/java/DAY2/gildedrose/GildedRose.java
+++ src/main/java/DAY2/gildedrose/GildedRose.java
@@ -49,15 +49,19 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                }
+            } else {
+                if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.sellIn < 0) {
                         item.quality = item.quality - item.quality;
-                    } else {
+                    }
+                } else {
+                    if (item.sellIn < 0) {
                         if (item.quality > 0) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
