faf4eb6067a1bffc913653ea54773bf11d18405d
youdongkwan <dkyou7@naver.com>
2024-12-10 13:37:11 +0900

    [2일차-오전] 선생님이 리팩토링 한 코드 2

--- src/main/java/DAY2/wheel/Wheel.java
+++ src/main/java/DAY2/wheel/Wheel.java
@@ -2,78 +2,101 @@ package DAY2.wheel;
 
 import java.util.ArrayList;
 
+class Axis {
+    int x, y;
+
+    public Axis(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class Wheel {
     public static final int MAX_TRY_CNT = 26;
+    public static final int MAX_QUIZ_LINE = 4;
+    public static final int MAX_QUIZ_LINE_LENGTH = 50;
+    //Wheel에서 주요 데이터
+    private ArrayList<StringBuilder> originAnswer;
+    //Wheel에서 State
+    private boolean[][] isFlip = new boolean[MAX_QUIZ_LINE][MAX_QUIZ_LINE_LENGTH];
+    private boolean[] isGettingSeoncdChance = new boolean[MAX_QUIZ_LINE];
+    private boolean[] isFinishFirstChance = new boolean[MAX_QUIZ_LINE];
+    private int continueAnswerCount = 0;
+    private int totalPrice = 0;
 
-    public static int getSum(ArrayList<StringBuilder> strs, String userdata) {
-        boolean[][] isFlip = new boolean[5][50];
-        int conCnt = 0;
-        int[] ffirst = {0, 0, 0, 0, 0};
-        int sum = 0;
-        int[] chance = {-1, -1, -1, -1, -1};
+    public Wheel(ArrayList<StringBuilder> originAnswer) {
+        this.originAnswer = originAnswer;
+    }
+
+    public int getTotalPrice(String userTotalAnswer) {
         //26글자 for 돌면서 퀴즈 참석자가 하나씩 시도를 하는 것
         for (int tryCnt = 0; tryCnt < MAX_TRY_CNT; tryCnt++) {
-            char tryChar = userdata.charAt(tryCnt);
-            //Let's Second 점수인지 확인한다.
-            for (int y = 0; y < strs.size(); y++) {
-                if (chance[y] == -1) continue;
-                for (int x = 0; x < strs.get(chance[y]).length(); x++) {
-                    if (isFlip[chance[y]][x] == true) continue;
-                    if (strs.get(chance[y]).charAt(x) != tryChar) continue;
-                    sum += 2000;
-                    break;
-                }
-                chance[y] = -1;
-            }
-            //Let's First 점수인지 확인한다.
-            for (int y = 0; y < strs.size(); y++) {
-                for (int x = 0; x < strs.get(y).length(); x++) {
-                    if (isFlip[y][x] == true) continue;
-                    if (strs.get(y).charAt(x) != tryChar) continue;
-                    if (ffirst[y] != 0) continue;
-                    ffirst[y] = 1;
-                    if (x == 0) {
-                        sum += 1000;
-                        chance[y] = y;
-                    }
-                }
-            }
-            //$100 단위 검사
-            int passCnt = getPassCnt(strs, isFlip, tryChar);
-            if (passCnt > 0) {
-                conCnt++;
-                sum += (conCnt * 100) * passCnt;
-            }
-            //맞춘것 뒤집기
-            flipCorrectChar(strs, isFlip, tryChar);
-            //맞춘게 하나도 없는 경우, 초기화
+            char userAnswer = userTotalAnswer.charAt(tryCnt);
+            ArrayList<Axis> axises = getAxis(userAnswer);
+            int passCnt = axises.size();
             if (passCnt == 0) {
-                conCnt = 0;
-                for (int t = 0; t < 5; t++) chance[t] = -1;
+                clearUpContinueData();
+                continue;
             }
+            continueAnswerCount++;
+            totalPrice += getUnitPrice(passCnt);
+            totalPrice += getSecondChancePrice(axises);
+            totalPrice += getFirstChancePrice(axises);
+            flipCorrectChar(axises);
         }
-        return sum;
+        return totalPrice;
     }
 
-    private static void flipCorrectChar(ArrayList<StringBuilder> strs, boolean[][] isFlip, char tryChar) {
-        for (int y = 0; y < strs.size(); y++) {
-            for (int x = 0; x < strs.get(y).length(); x++) {
+    private ArrayList<Axis> getAxis(char tryChar) {
+        ArrayList<Axis> result = new ArrayList<>();
+        for (int y = 0; y < originAnswer.size(); y++) {
+            for (int x = 0; x < originAnswer.get(y).length(); x++) {
                 if (isFlip[y][x] == true) continue;
-                if (strs.get(y).charAt(x) != tryChar) continue;
-                isFlip[y][x] = true;
+                if (originAnswer.get(y).charAt(x) != tryChar) continue;
+                result.add(new Axis(x, y));
             }
         }
+        return result;
     }
 
-    private static int getPassCnt(ArrayList<StringBuilder> strs, boolean[][] isFlip, char tryChar) {
-        int passCnt = 0;
-        for (int y = 0; y < strs.size(); y++) {
-            for (int x = 0; x < strs.get(y).length(); x++) {
-                if (isFlip[y][x] == true) continue;
-                if (strs.get(y).charAt(x) != tryChar) continue;
-                passCnt++;
+    private void clearUpContinueData() {
+        continueAnswerCount = 0;
+        isGettingSeoncdChance = new boolean[MAX_QUIZ_LINE];
+    }
+
+    private int getUnitPrice(int passCnt) {
+        return (continueAnswerCount * 100) * passCnt;
+    }
+
+    private int getSecondChancePrice(ArrayList<Axis> axises) {
+        int price = 0;
+        for (Axis axis : axises) {
+            if (!isGettingSeoncdChance[axis.y]) continue;
+            isGettingSeoncdChance[axis.y] = false;
+            price += 2000;
+        }
+        return price;
+    }
+
+    private int getFirstChancePrice(ArrayList<Axis> axises) {
+        isGettingSeoncdChance = new boolean[MAX_QUIZ_LINE];
+        int price = 0;
+        for (Axis axis : axises) {
+            int y = axis.y;
+            int x = axis.x;
+            if (isFinishFirstChance[y]) continue;
+            isFinishFirstChance[y] = true;
+            if (x == 0) {
+                price += 1000;
+                isGettingSeoncdChance[y] = true;
             }
         }
-        return passCnt;
+        return price;
+    }
+
+    private void flipCorrectChar(ArrayList<Axis> axises) {
+        for (Axis axis : axises) {
+            isFlip[axis.y][axis.x] = true;
+        }
     }
 }
